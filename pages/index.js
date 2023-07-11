f7c2e9b7c422437197d642a1243855390d50166b
kfoq7 <frbrizoi@gmail.com>
2023-07-11 13:10:57 -0500

    feat(page): Added source

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,13 @@
+import Link from 'next/link'
+import { AiFillGithub } from 'react-icons/ai'
+
 export default function Home() {
-  return <div>HomePage</div>
+  return (
+    <div className="flex items-center justify-center h-screen text-white">
+      <button className="flex items-center text-lg bg-gray-900 rounded-lg px-4">
+        <AiFillGithub className="mr-2" />
+        <Link href="https://github.com/kfoq7/math-project">Source Code</Link>
+      </button>
+    </div>
+  )
 }
